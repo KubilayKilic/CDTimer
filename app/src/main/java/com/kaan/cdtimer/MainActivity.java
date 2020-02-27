3b1c53112351019ccfb0334a81bb3fd7ce75edf0
SwordMaster3000 <kubilaykilich@gmail.com>
2020-02-27 20:01:33 +0300

    12 Second Counter

---
+++ app/src/main/java/com/kaan/cdtimer/MainActivity.java
@@ -0,0 +1,41 @@
+package com.kaan.cdtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//basit countdown timer ile toast mesajı ve bitişi mesajı
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Toast.makeText(getApplicationContext(), "Countdown started", Toast.LENGTH_LONG).show();
+
+        new CountDownTimer(12000,1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                textView.setText("Left: " + millisUntilFinished / 1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
+                textView.setText("Finished");
+            }
+        }.start();
+    }
+}
